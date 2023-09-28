9ab010db44c7c2c1ed93235116a56b72fbad0ee5
Eli Mullan <EliMullan21@gmail.com>
2023-09-28 11:06:00 +0100

    setup HomeScreen & Hero

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import Header from "../components/Header"
+import HomeScreen from "../screens/HomeScreen"
 
 const App = () => {
   return (
-   <>
-     <Header />
-     <h1> MERN Auth </h1>     
-   </>
-  )
+    <>
+      <Header />
+      <HomeScreen />
+    </>
+  );
 }
 
 export default App
