18015126faa268976a214dba5b522cc39551666a
Eli Mullan <EliMullan21@gmail.com>
2023-09-28 19:33:08 +0100

    setup authSlice.js for local stuff

--- frontend/store.js
+++ frontend/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./src/slices/AuthSlice";
+
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer  
+  },
   devTools: true
 });
 
